d093b32fa4400cb75c311b77d2b5848beec57ba5
LopesLs <97265556+LopesLs@users.noreply.github.com>
2024-01-09 15:36:18 -0300

    (refactor): change template name

--- website/urls.py
+++ website/urls.py
@@ -1,10 +1,8 @@
-from django.conf.urls.static import static
-from django.conf import settings
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path("home", views.SendVideoRequestHandler.as_view(), name="home"),
+    path("upload", views.SendVideoRequestHandler.as_view(), name="upload_video"),
     path("download/<int:pk>", views.download_audio, name="download_audio"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
